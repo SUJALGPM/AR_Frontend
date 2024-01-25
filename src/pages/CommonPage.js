9e9c6a69d69974d48a2b8023af2bcb9b52537886
SUJAL DINGANKAR <mailme.sujaldingankar@gmail.com>
2024-01-25 17:49:11 +0530

    no change

--- src/pages/CommonPage.js
+++ src/pages/CommonPage.js
@@ -6,7 +6,7 @@ const CommonPage = () => {
         <Layout>
             <div className='backimg_1' style={{ minHeight: "100%" }}>
                 <h1 className='text-center'>{"<<<<"}Welcome To our Project...{">>>>"}</h1>
-                
+
             </div>
         </Layout>
     )
